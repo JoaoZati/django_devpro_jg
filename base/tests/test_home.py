ce7abc280831584a718b9caf320388fd163479cd
JoaoZati <joaozati@gmail.com>
2021-09-06 19:13:07 -0300

    Adicionando URLs Nomeadas

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -1,6 +1,22 @@
 from django.test import Client
+import pytest
+
+from pypro.django_assertions import assert_contains
+from django.urls import reverse
+
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_titulo(resp):
+    assert_contains(resp, '<title>Python Pro João Guilherme</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("home")}">Python Pro JG</a>')
