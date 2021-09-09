116ccc5460871871f430ffc1a62516413d3fdb57
JoaoZati <joaozati@gmail.com>
2021-09-09 15:27:20 -0300

    Contexto de template

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação',
+             'vimeo_id': 600572543}
+    return render(request, 'aperitivos/video.html', context={'video': video})
