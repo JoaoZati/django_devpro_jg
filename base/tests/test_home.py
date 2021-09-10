c3d7c2c9974c2efd4a3872497d005648488ee1cf
JoaoZati <joaozati@gmail.com>
2021-09-10 08:49:34 -0300

    Composição de template

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -16,8 +16,12 @@ def test_status_code(resp):
 
 
 def test_titulo(resp):
-    assert_contains(resp, '<title>Python Pro João Guilherme</title>')
+    assert_contains(resp, '<title>Python Pro João Guilherme-Home</title>')
 
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro JG</a>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:ramalho@python.pro.br"')
