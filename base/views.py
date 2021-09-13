23e7460fe449ce96422888a62c5faffc938ebf22
JoaoZati <joaozati@gmail.com>
2021-09-13 09:11:25 -0300

    Processador de contextos

--- base/views.py
+++ base/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse # noqa
-from django.shortcuts import render # noqa
+from django.shortcuts import render
+
+from modulos import facade
 
 
 def home(request):
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', {'MODULOS': facade.listar_modulos_ordenados()})
