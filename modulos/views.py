4ae2241f0babe457194b6a0f830190c522b9b55c
JoaoZati <joaozati@gmail.com>
2021-09-14 10:41:01 -0300

    Aula: Adição de coluna em Tabela -Criado o link com as Aulas =)

--- modulos/views.py
+++ modulos/views.py
@@ -6,3 +6,8 @@ def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.lista_aulas_de_modulo_ordenadas(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    aula = facade.encontrar_aula(slug)
+    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
